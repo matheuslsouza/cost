1fec3e477e5fc961f6aa265391ee2dd210d7a24b
Matheus Lourenço de Souza <107487181+matheuslsouza@users.noreply.github.com>
2022-08-24 16:56:18 -0300

    Add files via upload

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,17 @@
+import styles from "./Home.module.css";
+import savings from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={styles.home_container}>
+      <h1>
+        Bem-Vindo ao <span>Cost</span>
+      </h1>
+      <p>Comece a gerenciar ao seus projetos agora mesmo !</p>
+      <LinkButton to="/NewProject" text="Criar Projeto" />
+      <img src={savings} alt="Cost" />
+    </section>
+  );
 }
 export default Home;
